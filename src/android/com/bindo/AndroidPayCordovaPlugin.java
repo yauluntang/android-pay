4860c0f94b208b2d7f08ff7fef2c349879a88034
yaulun.tang <yaulun.tang@gmail.com>
2017-08-01 22:21:52 -0400

    'test'

--- src/android/com/bindo/AndroidPayCordovaPlugin.java
+++ src/android/com/bindo/AndroidPayCordovaPlugin.java
@@ -25,8 +25,76 @@ public class AndroidPayCordovaPlugin extends CordovaPlugin {
     Log.d(TAG, "Initializing AndroidPayCordovaPlugin");
   }
 
+  @Override
+  public void onConnectionFailed(ConnectionResult result) {
+      // An unresolvable error has occurred and a connection to Google APIs
+      // could not be established. Display an error message, or handle
+      // the failure silently
+       // ...
+  }
+
+  public void walletRequest(){
+        String publicKey = "BNSUODPqWgPLRPp368MkTJIH+HAgNGQ5l61BCKeKe7xxteZoDR2M7jVLLY9H7Ai8OdS5Q1CLpBFXv2LLMb1sHxU=";
+        PaymentMethodTokenizationParameters parameters =
+          PaymentMethodTokenizationParameters.newBuilder()
+              .setPaymentMethodTokenizationType(PaymentMethodTokenizationType.NETWORK_TOKEN)
+              .addParameter("publicKey", publicKey)
+              .build();
+
+        MaskedWalletRequest request = MaskedWalletRequest.newBuilder()
+           .setMerchantName(Constants.MERCHANT_NAME)
+           .setPhoneNumberRequired(true)
+           .setShippingAddressRequired(true)
+           .setCurrencyCode(Constants.CURRENCY_CODE_USD)
+           .setEstimatedTotalPrice(cartTotal)
+                   // Create a Cart with the current line items. Provide all the information
+                   // available up to this point with estimates for shipping and tax included.
+           .setCart(Cart.newBuilder()
+                   .setCurrencyCode(Constants.CURRENCY_CODE_USD)
+                   .setTotalPrice(cartTotal)
+                   .setLineItems(lineItems)
+                   .build())
+           .setPaymentMethodTokenizationParameters(parameters)
+           .build();
+
+           cordova.setActivityResultCallback (this);
+  }
+
+  public void checkAndroidPayAvailability() {
+    GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
+      .enableAutoManage(this /* FragmentActivity */,
+                        this /* OnConnectionFailedListener */)
+      .addApi(Drive.API)
+      .addScope(Drive.SCOPE_FILE)
+      .build();
+    Wallet.Payments.isReadyToPay(mGoogleApiClient).setResultCallback( new ResultCallback<BooleanResult>() {
+                  @Override
+                  public void onResult(@NonNull BooleanResult booleanResult) {
+
+
+                      if (booleanResult.getStatus().isSuccess()) {
+                          if (booleanResult.getValue()) {
+                                walletRequest();
+                          } else {
+
+                          }
+                      } else {
+                          // Error making isReadyToPay call
+                          Log.e(TAG, "isReadyToPay:" + booleanResult.getStatus());
+                      }
+                  }
+          });
+  }
   public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+
+    if ( action.equals("pay")){
+        checkAndroidPayAvailability();
+    }
+
     if(action.equals("echo")) {
+
+
+
       String phrase = args.getString(0);
       // Echo back the first argument
       Log.d(TAG, phrase);
